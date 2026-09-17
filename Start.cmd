14bc29c87476d12fe6e1189a1c0ad11aa0e68f88
agilecoach-ashutosh <agilecoach.ashutosh@gmail.com>
2026-09-17 09:25:19 +0530

    Launch Presence role-selection practice mode

--- Start.cmd
+++ Start.cmd
@@ -5,4 +5,4 @@ if not exist ".venv\Scripts\pythonw.exe" (
   pause
   exit /b 1
 )
-start "Presence Coach" ".venv\Scripts\pythonw.exe" "launch.py"
+start "Presence Coach" ".venv\Scripts\pythonw.exe" "practice_mode.py"
