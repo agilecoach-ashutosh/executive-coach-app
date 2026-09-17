66688bcf8f49932f68445695920ef1a25c08104e
agilecoach-ashutosh <agilecoach.ashutosh@gmail.com>
2026-09-17 15:29:16 +0530

    Launch Word and MP3 export layer

--- Start.cmd
+++ Start.cmd
@@ -5,4 +5,4 @@ if not exist ".venv\Scripts\pythonw.exe" (
   pause
   exit /b 1
 )
-start "Presence Coach" ".venv\Scripts\pythonw.exe" "provider_mode.py"
+start "Presence Coach" ".venv\Scripts\pythonw.exe" "session_export_mode.py"
